cbe0886004a5ad79f5291d33e71019cc058224d2
knmanikandan <knmanikandan71@gmail.com>
2022-09-19 17:07:19 +0530

    @ 5:08PM on 19/09/22

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask,render_template
+import sqlite3
 
-app=Flask(__name__)
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_login import LoginManager
+
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired
+#Flask instance
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisismani'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), nullable = False)
+    password = db.Column(db.String(80), nullable = False)
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/support')
 def support():
@@ -17,8 +46,7 @@ def about():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404    
-
+    return render_template('404.html'), 404   
 
 if __name__=='__main__':
     app.run(debug=True) 
